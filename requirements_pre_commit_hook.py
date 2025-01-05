8c7cde3bc68e261d69dcb36e1dea45b7e1a4a31c
redside100 <andrewpeng00@gmail.com>
2025-01-04 21:43:07 -0500

    feat: add requirements pre-commit hook

---
+++ requirements_pre_commit_hook.py
@@ -0,0 +1,31 @@
+import os
+import tempfile
+import filecmp
+import difflib
+
+
+def check_requirements():
+    # Freeze the current environment's dependencies into a temp file.
+    tmp = tempfile.NamedTemporaryFile()
+    tmp.close()
+    os.system(f"pip freeze > {tmp.name}")
+
+    # Check the diff between the temp requirements file and real requirements.txt.
+    with open("requirements.txt", "r") as real_req, open(tmp.name, "r") as tmp_req:
+        diff = difflib.context_diff(
+            tmp_req.read().splitlines(1), real_req.read().splitlines(1)
+        )
+
+    # If the generator is exhausted, then there is no diff.
+    # Otherwise, there is a diff.
+    _exhausted = object()
+
+    if next(diff, _exhausted) is not _exhausted:
+        print("".join(diff))
+        raise AssertionError(
+            "Mismatch between current environment and frozen requirements.txt. Please update requirements.txt before committing!"
+        )
+
+
+if __name__ == "__main__":
+    check_requirements()
